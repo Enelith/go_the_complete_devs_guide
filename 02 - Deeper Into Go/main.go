d9571ee7737988a6a7c57e34369857f456b864ce
VIVERIS\j.vinh <jonathan.vinh@viveris.fr>
2025-12-29 14:06:47 +0100

    2.17 - Functions and Return Types

--- 02 - Deeper Into Go/main.go
+++ 02 - Deeper Into Go/main.go
@@ -4,8 +4,13 @@ import "fmt"
 
 func main() {
 	//var card string = "Ace of Spades"
-	card := "Ace of Spades"   // New Variable
-	card = "Five of Diamonds" // Variable assignment
+	//card := "Ace of Spades" // New Variable
+	//card = "Five of Diamonds" // Variable assignment
+	card := newCard()
 
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
