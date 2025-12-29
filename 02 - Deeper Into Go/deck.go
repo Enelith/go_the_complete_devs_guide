19bf38208cd241b5aa095ed07d129c6a83ce8cae
VIVERIS\j.vinh <jonathan.vinh@viveris.fr>
2025-12-29 15:18:50 +0100

    2.22 Creating a New Deck

--- 02 - Deeper Into Go/deck.go
+++ 02 - Deeper Into Go/deck.go
@@ -1,8 +1,25 @@
 package main
 
+import "fmt"
+
 // Create a new type of 'deck'
 // which is a slice of strings.
 type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	// Are slices of String
+	cardSuits := []string{"Hearts", "Diamonds", "Clubs", "Spades"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, fmt.Sprintf("%s of %s", value, suit))
+		}
+	}
+	return cards
+}
 
 func (d deck) print() {
 	for index, card := range d {
--- 02 - Deeper Into Go/main.go
+++ 02 - Deeper Into Go/main.go
@@ -1,12 +1,7 @@
 package main
 
 func main() {
-	cards := deck{newCard(), "Ace of Diamonds"}
-	cards = append(cards, "Six of Spades") // Add an element to the end of the slice
+	cards := newDeck()
 
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
