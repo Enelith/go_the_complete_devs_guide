9b44df63dea03eee7c9560ddf92ea3dfd5a1d696
VIVERIS\j.vinh <jonathan.vinh@viveris.fr>
2026-01-05 15:46:59 +0100

    5.69 Working with the Read Function, 5.70 The Writer Interface, 5.73 A Custom Writer

--- 05 - Interfaces/05.3 - Http/main.go
+++ 05 - Interfaces/05.3 - Http/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://www.google.com")
@@ -14,5 +17,27 @@ func main() {
 		//panic(err)
 	}
 
-	fmt.Println(resp)
+	// 1) Using the READ method
+	// Creating an empty byte slice large enough to hold the response body (if too small, the Reader will stop early)
+	//bs := make([]byte, 99999)
+	//resp.Body.Read(bs)
+
+	// Will therefore display the response body html
+	//fmt.Println(string(bs))
+
+	// 2) Using the COPY method
+	//io.Copy(os.Stdout, resp.Body)
+
+	// 3) Using a Custom Writer
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes: ", len(bs))
+
+	// Returning the length of the byte slice we consumed through the fmt.Println
+	return len(bs), nil
 }
