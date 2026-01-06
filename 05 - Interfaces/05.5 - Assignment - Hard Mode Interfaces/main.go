6b748ef3313defc28a1b68ad80fd6b7f9f628160
VIVERIS\j.vinh <jonathan.vinh@viveris.fr>
2026-01-06 11:41:01 +0100

    5.77 Assignment: Hard Mode Interfaces (Solution)

--- 05 - Interfaces/05.5 - Assignment - Hard Mode Interfaces/main.go
+++ 05 - Interfaces/05.5 - Assignment - Hard Mode Interfaces/main.go
@@ -1,4 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
 func main() {
+	fmt.Println(os.Args[1])
+
+	fileName := os.Args[1]
+
+	f, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	defer func(f *os.File) {
+		_ = f.Close()
+	}(f)
+
+	// Note that os.File implements io.Reader
+	_, _ = io.Copy(os.Stdout, f)
 }
