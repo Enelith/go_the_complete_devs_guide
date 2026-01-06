ab95d5915e198dee1e00b294b92e595c29848497
VIVERIS\j.vinh <jonathan.vinh@viveris.fr>
2026-01-06 11:16:29 +0100

    5.75 Assignment: Interfaces (Solution)

--- 05 - Interfaces/05.4 - Assignment - Interfaces/main.go
+++ 05 - Interfaces/05.4 - Assignment - Interfaces/main.go
@@ -1,4 +1,36 @@
 package main
 
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type square struct {
+	sideLength float64
+}
+type triangle struct {
+	base   float64
+	height float64
+}
+
 func main() {
+	// Testing:
+	s := square{sideLength: 10}
+	t := triangle{base: 10, height: 10}
+
+	printArea(s)
+	printArea(t)
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func (t triangle) getArea() float64 {
+	return (t.base * t.height) / 2
+}
+
+func printArea(s shape) {
+	fmt.Println("Area: ", s.getArea())
 }
